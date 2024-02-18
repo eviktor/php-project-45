a31fb886eca9972ddbec5b70a56d57995fb5a8ad
Viktor K <vk7771199@gmail.com>
2024-02-18 21:02:35 +0700

    feat: Add GCD Game

---
+++ src/Games/GCDGame.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BrainGames\Games\GCDGame;
+
+use function BrainGames\Engine\playGame;
+use function BrainGames\GCD\getGCD;
+
+function generateQuestionCallback(): array
+{
+    $number1 = rand(1, 100);
+    $number2 = rand(1, 100);
+
+    $question = "$number1 $number2";
+    $correctAnswer = getGCD($number1, $number2);
+    return [ $question, "$correctAnswer" ];
+}
+
+function runGame()
+{
+    playGame(
+        'Find the greatest common divisor of given numbers.',
+        'BrainGames\Games\GCDGame\generateQuestionCallback'
+    );
+}
