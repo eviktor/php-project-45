49c2230222a942790f212c17549b120e6a185231
Viktor K <vk7771199@gmail.com>
2024-02-26 19:13:42 +0700

    refactor: Replace eval function using with own function

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -6,6 +6,19 @@ use function BrainGames\Engine\run;
 
 use const BrainGames\Engine\MAX_RANDOM_NUMBER;
 
+function evalExpression(int $a, int $b, string $operation): int
+{
+    switch ($operation) {
+        case '+':
+            return $a + $b;
+        case '-':
+            return $a - $b;
+        case '*':
+            return $a * $b;
+    }
+    return 0;
+}
+
 function buildQuestionCallback(): array
 {
     $a = rand(1, MAX_RANDOM_NUMBER);
@@ -14,7 +27,7 @@ function buildQuestionCallback(): array
     $operation = $operations[array_rand($operations)];
 
     $expression = "$a $operation $b";
-    $correctAnswer = eval("return ($expression);");
+    $correctAnswer = evalExpression($a, $b, $operation);
 
     return [ $expression, "$correctAnswer" ];
 }
